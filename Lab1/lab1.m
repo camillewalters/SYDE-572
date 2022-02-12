38cd288575bfdd84c8355f2b42cfcaf3b72ccffc
camillewalters <camille.h.walters@gmail.com>
2022-02-12 14:25:48 -0500

    Generate two figures

--- Lab1/lab1.m
+++ Lab1/lab1.m
@@ -2,21 +2,23 @@ clear;
 close all;
 clf;
 
-% muA = [5 10]';
-% sigmaA = [8 0 ; 0 4];
-% generate_and_plot(muA, sigmaA,200, 'r')
-% 
-% hold on;
-% 
-% muB = [10 15]';
-% sigmaB = [8 0 ; 0 4];
-% generate_and_plot(muB, sigmaB, 200, 'b')
-% 
-% xlabel('X_1')
-% ylabel('X_2')
-% title("Case 1 Clusters")
-% legend("Class A", "Class A Standard Deviation Contour", "Class B", "Class B Standard Deviation Contour")
+figure(1);
+muA = [5 10]';
+sigmaA = [8 0 ; 0 4];
+generate_and_plot(muA, sigmaA,200, 'r')
 
+hold on;
+
+muB = [10 15]';
+sigmaB = [8 0 ; 0 4];
+generate_and_plot(muB, sigmaB, 200, 'b')
+
+xlabel('X_1')
+ylabel('X_2')
+title("Case 1 Clusters")
+legend("Class A", "Class A Standard Deviation Contour", "Class B", "Class B Standard Deviation Contour", 'Location', 'best')
+
+figure(2);
 muC = [5 10]';
 sigmaC = [8 4; 4 40];
 generate_and_plot(muC, sigmaC, 100, 'r')
@@ -36,3 +38,5 @@ generate_and_plot(muE, sigmaE, 150, 'g')
 xlabel('X_1')
 ylabel('X_2')
 title("Case 2 Clusters")
+legend("Class C", "Class C Standard Deviation Contour", "Class D", "Class D Standard Deviation Contour", "Class E", "Class E Standard Deviations Contour", 'Location', 'best')
+
